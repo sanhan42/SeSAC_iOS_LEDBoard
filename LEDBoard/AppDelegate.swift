bacccec2763d0ee12be79ad6529fdc0c2f9e18a4
sanhan42 <sangmin613@gmail.com>
2022-07-07 18:08:43 +0900

    first commit

--- LEDBoard/AppDelegate.swift
+++ LEDBoard/AppDelegate.swift
@@ -9,22 +9,32 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        sleep(1)
         return true
+    }
+    
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // youtube 예시 - 사용자 프리미엄 결제 했다면 play, 그렇지 않으면 일시정지
+        // 금융앱 - 백그라운드 상태일 때, 정보 보호를 위해 화면 가리기
+    }
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // youtube 예시 - 프리미엄 사용안하는 유저에게 결제 유도 팝업.
+        // kakaotalk 예시 - 비밀번호 요구
     }
 
     // MARK: UISceneSession Lifecycle
-
+    @available (iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    @available (iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
---
+++ LEDBoard/BoardViewController.swift
@@ -0,0 +1,96 @@
+//
+//  BoardViewController.swift
+//  LEDBoard
+//
+//  Created by 한상민 on 2022/07/06.
+//
+
+import UIKit
+
+class BoardViewController: UIViewController {
+    @IBOutlet weak var userTextFeild: UITextField!
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var textColorButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet var buttonList: [UIButton]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //designButton()
+        studyOutletCollection(list: buttonList)
+        specificDesignButton(sendButton, buttonTitle: "바꾸기", highlightedTitle: "빨리")
+        specificDesignButton(textColorButton, buttonTitle: "색 바꾸기", highlightedTitle: "완료")
+        designTextField()
+        resultLabel.numberOfLines = 0
+        // Do any additional setup after loading the view.
+    }
+    
+    func designTextField() {
+        userTextFeild.placeholder = "내용을 입력해주세요"
+        userTextFeild.textColor = .brown
+        userTextFeild.keyboardType = .emailAddress
+        userTextFeild.adjustsFontSizeToFitWidth = true
+    }
+    
+    func designButton() {
+        sendButton.setTitle("보내기", for: .normal)
+        sendButton.setTitle("빨리!!", for: .highlighted)
+        sendButton.backgroundColor = .yellow
+        sendButton.layer.cornerRadius = 8
+        sendButton.layer.borderColor = UIColor.black.cgColor
+        sendButton.layer.borderWidth = 3
+        sendButton.setTitleColor(.red, for: .normal)
+        sendButton.setTitleColor(.blue, for: .highlighted)
+    }
+    
+    func specificDesignButton(_ button:UIButton, buttonTitle t1:String, highlightedTitle t2:String){
+        button.setTitle(t1, for: .normal)
+        button.setTitle(t2, for: .highlighted)
+    }
+    
+    func studyOutletCollection(list:[UIButton]) {
+        for item in list {
+            item.backgroundColor = .yellow
+            item.layer.cornerRadius = 8
+            item.layer.borderColor = UIColor.black.cgColor
+            item.layer.borderWidth = 3
+            item.setTitleColor(.red, for: .normal)
+            item.setTitleColor(.blue, for: .highlighted)
+        }
+//        // 1. 배열, 반복문 활용
+//        let buttonArray: [UIButton] = [sendButton, textColorButton]
+//
+//        for item in buttonArray {
+//            item.backgroundColor = .blue
+//            item.layer.cornerRadius = 2
+//        }
+//        // 2. 아웃렛 컬렉션
+//        for item in buttonList {
+//            item.backgroundColor = .blue
+//            item.layer.cornerRadius = 2
+//        }
+    }
+    
+    @IBAction func sendButtonClicked(_ sender: UIButton) {
+//        print(sendButton.currentTitle)
+        resultLabel.text = userTextFeild.text
+    }
+    
+    @IBAction func tapGestureClicked(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func exampleButtonsClicked(_ sender: UIButton) {
+        view.endEditing(true)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
